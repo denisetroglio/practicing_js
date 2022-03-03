8c338c8dbc58c12797a6802e912acbda64e83e6c
Denise Troglio <overbeck.denise@gmail.com>
2022-03-03 09:28:28 +0100

    Add new exercice innerHTML

---
+++ main.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const Title = document.querySelector(".js_title");
+Title.innerHTML="Practicing JS";
+
+/* Ejercicio 4
+Seleccionando Adalabers:
+Hay que crear una página HTML que contenga un listado con tu nombre y el de tu compañera, y un título que diga "La Adalaber seleccionada es: ". Usando JavaScript, tenemos que cambiar el título añadiendo el nombre del primer li. */
+
+
+const Adalaber = document.querySelector(".js_adalaber");
+Adalaber.innerHTML = "La Adalaber seleccionadda es:";
+
+Adalaber.innerHTML = Adalaber.innerHTML + "Denise Troglio";
+
+//------------------------------------------------------------------------//
+
+
+
+
