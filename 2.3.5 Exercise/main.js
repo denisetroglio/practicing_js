962f5c739b3b491f92272b92587fa75b0622f8cd
Denise Troglio <overbeck.denise@gmail.com>
2022-03-05 20:02:49 +0100

    add rainbow notification exercise

---
+++ 2.3.5 Exercise/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/*
+Si contiene la clase warning, el título sea 'AVISO' y el texto sea: 'Tenga cuidado'
+Si contiene la clase error, el título sea 'ERROR' y el texto sea: 'Ha surgido un error'
+Si contiene la clase success, el título sea 'CORRECTO' y el texto sea: 'Los datos son correctos'*/
+
+const notificationMessage = document.querySelector(".js_notification");
+const title = document.querySelector(".js_title");
+const paragraph = document.querySelector(".js_paragraph");
+
+if (notificationMessage.classList.contains("warning")) {
+  title.innerHTML = `AVISO`;
+  paragraph.innerHTML = "¡Tenga cuidado!";
+} else if (notificationMessage.classList.contains("error")) {
+  title.innerHTML = `ERROR`;
+  paragraph.innerHTML = "¡Ha surgido un error!";
+} else if (notificationMessage.classList.contains("success")) {
+  title.innerHTML = `CORRECTO`;
+  paragraph.innerHTML = "Los datos son correctos";
+};
