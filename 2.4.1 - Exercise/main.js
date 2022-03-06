5ec6568fa20364c930b5b64cec560b72f910d0ad
Denise Troglio <overbeck.denise@gmail.com>
2022-03-06 12:07:21 +0100

    add
    even or odd exercise

--- 2.4.1 - Exercise/main.js
+++ 2.4.1 - Exercise/main.js
@@ -1,11 +1,21 @@
-"use strict";
+// "use strict";
 
-const multResult = mul(40, 5);
+// const multResult = mul(40, 5);
 
-function mul(a,b) {
-  const result = a * b;
+// function mul(a,b) {
+//   const result = a * b;
 
-  return result;
+//   return result;
+// }
+
+// console.log(multResult);
+
+//Número ímpar o par:
+
+let num = 41;
+
+if(num%2==0){
+    console.log("este número es par");
+}else{
+  console.log("este número es impar");
 }
-
-console.log(multResult);
---
+++ 2.4.1 - Exercise/par_impar.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let num = 1;
+
+if(num % 3){
+    "este número es ímpar";
+}else{
+    "este número es par";
+}
